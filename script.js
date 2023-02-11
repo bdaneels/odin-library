0cdfd643e8639f00b1473123eebf9cb4f7a6792d
bdaneels <brecht.daneels@hotmail.com>
2023-02-11 13:29:23 +0100

    change function eventlistner based on id to classes

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let myLibrary = [];
 const libraryDiv = document.querySelector("#library");
-const submitBtn = document.querySelector("#btn")
-const titleField = document.querySelector("#title")
-const authorField = document.querySelector("#author")
-const pagesField = document.querySelector("#pages")
-const readField = document.querySelector("#read")
-let readBtns = document.querySelectorAll("#read-btn")
-let unreadBtns = document.querySelectorAll("#unread-btn")
-let removeBtns = document.querySelectorAll("#remove")
+const submitBtn = document.querySelector("#btn");
+const titleField = document.querySelector("#title");
+const authorField = document.querySelector("#author");
+const pagesField = document.querySelector("#pages");
+const readField = document.querySelector("#read");
+let readBtns = document.querySelectorAll("#readbtn");
+let removeBtns = document.querySelectorAll("#remove");
 
 /* object */
 function Book(title, author, pages, read) {
@@ -21,7 +20,7 @@ function Book(title, author, pages, read) {
 /*  */
 /* functions */
 function showBooksInLibrary() {
-  libraryDiv.innerHTML = ""
+  libraryDiv.innerHTML = "";
   for (i in myLibrary) {
     let book = myLibrary[i];
     /* create dom elements */
@@ -35,140 +34,133 @@ function showBooksInLibrary() {
     bookDiv.setAttribute("data", `${i}`);
     bookDiv.setAttribute("class", "book-card");
 
-
-
     /* add content */
     titleHeader.textContent = book.title;
     authorSpan.textContent = book.author;
     pagesDiv.textContent = book.pages;
-    removeDiv.setAttribute("id", "remove")
-    removeDiv.textContent="Delete book"
+    removeDiv.setAttribute("id", "remove");
+    removeDiv.textContent = "Delete book";
 
     if (book.read) {
-    readDiv.textContent = "Read"
-    readDiv.setAttribute("id", "read-btn")
-  } else {
-    readDiv.textContent = "Unread"
-    readDiv.setAttribute("id", "unread-btn")
-  }
+      readDiv.textContent = "Read";
+      readDiv.setAttribute("class", "read"); /* class adden */
+    } else {
+      readDiv.textContent = "Unread";
+      readDiv.setAttribute("class", "unread");
+    }
 
-    bookDiv.appendChild(titleHeader)
-    bookDiv.appendChild(authorSpan)
-    bookDiv.appendChild(pagesDiv)
-    bookDiv.appendChild(readDiv)
-    bookDiv.appendChild(removeDiv)
+    readDiv.setAttribute("id", "readbtn");
 
+    bookDiv.appendChild(titleHeader);
+    bookDiv.appendChild(authorSpan);
+    bookDiv.appendChild(pagesDiv);
+    bookDiv.appendChild(readDiv);
+    bookDiv.appendChild(removeDiv);
 
     libraryDiv.appendChild(bookDiv);
-    setEventListeners()
+    selectReadButtons();
+    setEventListeners();
   }
 }
 
 /* note: arry number is used in the for loop */
 
-
 /* checkbox validation */
-function checkboxStatus () {
+function checkboxStatus() {
   if (readField.checked) {
-    return true
+    return true;
+  } else {
+    return false;
   }
-  else {
-    return false
-  };
 }
 
 /* target the submit button and override default */
-submitBtn.addEventListener("click", submitClick, false)
+submitBtn.addEventListener("click", submitClick, false);
 
 function submitClick(event) {
   event.preventDefault();
-  let title = titleField.value
-  let author = authorField.value
-  let pages = pagesField.value
-  let read = checkboxStatus()
-  let book = new Book(title, author, pages, read)
-  myLibrary.push(book)
-  showBooksInLibrary()
+  let title = titleField.value;
+  let author = authorField.value;
+  let pages = pagesField.value;
+  let read = checkboxStatus();
+  let book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  showBooksInLibrary();
 }
 
 /* update buttons */
 
 function selectReadButtons() {
- readBtns = document.querySelectorAll("#read-btn")
- unreadBtns = document.querySelectorAll("#unread-btn")
- removeBtns = document.querySelectorAll("#remove")
+  readBtns = document.querySelectorAll("#readbtn");
+  removeBtns = document.querySelectorAll("#remove");
 }
 
 /* set event listeners */
 
-function setEventListeners(){
-  setEventListenersRead()
-  setEventListenersUnread()
-  setEventListenersRemove()
+function setEventListeners() {
+  selectReadButtons();
+  setEventListenersRead();
+  setEventListenersRemove();
 }
 
+function readBtnEvent(button) {
+  console.log("i've been clicked");
+  index = button.parentElement.getAttribute("data");
+  changeReadStatus(index);
+  if (button.classList.contains("read")) {
+    button.classList.remove("read");
+    button.classList.add("unread");
+    button.textContent = "Unread";
+  } else if (button.classList.contains("unread")) {
+    button.classList.remove("unread");
+    button.classList.add("read");
+    button.textContent = "Read";
+  }
+}
 
-function setEventListenersRead(){
-  selectReadButtons()
+function setEventListenersRead() {
+  console.log(readBtns);
   readBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-      button.setAttribute("id", "unread-btn")
-      button.textContent = "Unread"
-      index = button.parentElement.getAttribute('data')
-      changeReadStatus(index)
-      setEventListenersUnread()
-    })
-  })
+    button.addEventListener(
+      "click",
+      function () {
+        readBtnEvent(button);
+      },
+      false
+    );
+  });
 }
 
-function setEventListenersUnread() {
-  selectReadButtons()
-  unreadBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-      button.setAttribute("id", "read-btn")
-      button.textContent = "Read"
-      index = button.parentElement.getAttribute('data')
-      changeReadStatus(index)
-      setEventListenersRead()
-
-    })
-  })
-}
-
-function setEventListenersRemove(){
-  selectReadButtons()
-removeBtns.forEach((button) => {
-  button.addEventListener('click', () => {
-          
-    index = button.parentElement.getAttribute('data')
-    deleteBook(index)
-  })
-})
+function setEventListenersRemove() {
+  removeBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+      index = button.parentElement.getAttribute("data");
+      deleteBook(index);
+    });
+  });
 }
 
 /* change read status */
 
 function changeReadStatus(index) {
-  let book = myLibrary[index]
-  readStatus = book.read
+  let book = myLibrary[index];
+  readStatus = book.read;
   if (readStatus) {
-    book.read = false 
+    book.read = false;
   } else {
-    book.read = true
+    book.read = true;
   }
-  console.log("changed")
+  console.log("changed");
 }
 
 /* delete book */
 
 function deleteBook(index) {
-  delete myLibrary[index]
-  showBooksInLibrary()
+  delete myLibrary[index];
+  showBooksInLibrary();
 }
 
 /* test variables stuff */
-
-
 
 /* run the program */
 showBooksInLibrary();
